ede1c64d8ab2cc617303a763e951f36956ce94b0
BrauUu <brayanfrank.sm@gmail.com>
2024-02-04 15:38:29 -0300

    create contact section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -56,6 +56,83 @@ function getYearsDifference(date) {
     
 }
 
+const [nameInput, emailInput, messageInput] = document.querySelectorAll("#form > .field > .input")
+const form = document.querySelector("#form")
+
+nameInput.addEventListener('blur' , () => {
+    validateLenght(nameInput)
+})
+
+emailInput.addEventListener('blur' , () => {
+    validateLenght(emailInput)
+    validateEmail(emailInput)
+})
+
+messageInput.addEventListener('blur' , () => {
+    validateLenght(messageInput)
+})
+
+form.addEventListener("submit", (event) => {
+    validateSubmit(event)
+})
+
+function validateSubmit(event){
+    let error = false
+    if(!validateLenght(nameInput))
+        error = true
+    if(!validateLenght(emailInput) || !validateEmail(emailInput))
+        error = true
+    if(!validateLenght(messageInput))
+        error = true
+
+    if(error)
+        event.preventDefault();
+    
+}
+
+function validateLenght(field) {
+    const help_text = document.querySelector(`.${field.name.concat('_help')}`)
+    const fieldLength = field.value.length
+    const min = field.getAttribute('min')
+    const max = field.getAttribute('max')
+
+    if (fieldLength == 0){
+        help_text.textContent = "Campo obrigatório!"
+        field.classList.add("invalid-input")
+        return false
+    } else {
+        if(fieldLength < min){
+            help_text.textContent = `Valor mínimo de ${min} caracteres!`
+            field.classList.add("invalid-input")
+            return false
+        }
+        else if(fieldLength > max) {
+            help_text.textContent = `Valor máximo de ${max} caracteres!`
+            field.classList.add("invalid-input")
+            return false
+        }
+        help_text.textContent = ""
+        field.classList.remove("invalid-input")
+        return true   
+    }
+}
+
+function validateEmail(field) {
+    const help_text = document.querySelector(`.${field.name.concat('_help')}`)
+
+    if (field.value.length > 0 ){
+        if (field.validity.typeMismatch) {
+            help_text.textContent = "E-mail inválido!"
+            field.classList.add("invalid-input")
+            return false
+        } else {
+            help_text.textContent = ""
+            field.classList.remove("invalid-input")
+            return true
+        }
+    }
+}
+
 const ageEl = document.querySelector("#age")
 ageEl.textContent = getYearsDifference('2001-04-15')
 
